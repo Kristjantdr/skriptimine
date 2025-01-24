db7a3208d717736fdfe1cacfbce6cd32e06d1416
Kristjan Teder <kristjan.teder@voco.ee>
2025-01-24 14:35:41 +0200

    phpmyadmin muudatused

--- praks7/phpmyadmin_paigaldus.sh
+++ praks7/phpmyadmin_paigaldus.sh
@@ -2,8 +2,8 @@
 #phpmyadmin paigaldus Ubuntus
 
 #Uuendame seadet
-#sudo apt update -y
-#sudo apt upgrade -y 
+sudo apt update -y
+sudo apt upgrade -y 
 
 #kontrollime kas mariadb on paigaldatud
 MARIA=$(dpkg-query -W -f='${Status}' mariadb 2>/dev/null | grep -c 'ok installed')
@@ -13,7 +13,7 @@ if [ $MARIA -eq 0 ]; then
 
 #väljastame teate ja paigaldame teenuse
 echo "Paigaldame mariadb ja vajalikud lisad"
-apt install mariadb-server-10.6 mariadb-client-10.6
+sudo apt install -y mariadb-server mariadb-client
 echo "mariadb on paigaldatud!"
 
 #kui väärtus võrdub 1-ga
@@ -30,27 +30,28 @@ echo "Järgmisena paigaldame phpmyadmin"
 sleep 3
 
 #kontrollime kas phpmyadmin on juba paigaldatud
-#PMA=$(dpkg-query -W -f='${Status}' phpmyadmin 2>/dev/null | grep -c 'ok installed')
+PMA=$(dpkg-query -W -f='${Status}' phpmyadmin 2>/dev/null | grep -c 'ok installed')
 
 #kui väärtus võrdub 0-ga
-#if [ $PMA -eq 0 ]; then
+if [ $PMA -eq 0 ]; then
 
 #väljastame teate ja paigaldame teenuse
 echo "Paigaldame phpmyadmin ja vajalikud lisad"
-#apt install phpmyadmin -y
+sudo apt install phpmyadmin -y
 echo "phpmyadmin on paigaldatud"
 
 #kui väärtus võrdub 1-ga
-#elif [ $PMA -eq 1 ]; then
+elif [ $PMA -eq 1 ]; then
 
 #väljastame teate, et teenus on paigaldatud
 echo "phpmyadmin on juba paigaldatud!"
 
-#käivitame ja näitame staatust
-#service phpmyadmin start
-#service phpmyadmin status
-
-#lõpetame skripti
 fi
+
+#käivitame ja näitame staatust
+#sudo systemctl phpmyadmin start
+#sudo systemctl phpmyadmin status
+
+
 
 #skripti lõpp
