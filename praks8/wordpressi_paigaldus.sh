63f3f3aa8716240427dc86c437a962c14f64c776
Kristjan Teder <kristjan.teder@voco.ee>
2025-01-26 12:24:36 +0200

    wordpressi paigaldamine 2

--- praks8/wordpressi_paigaldus.sh
+++ praks8/wordpressi_paigaldus.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 # Wordpressi paigaldus
 
+# Kontrollime, kas skripti käivitab root kasutaja
+if [ "$(id -u)" -ne 0 ]; then
+    echo "Palun käivitage skript root kasutajana või sudo abil."
+    exit 1
+fi
+
+#Kontrollime ja paigaldame uuendused
+apt update && apt install -y
+
 #kontrollime kas apache2 on installitud.
 APACHE2=$(dpkg-query -W -f='${Status}' apache2 2>/dev/null | grep -c 'ok installed')
 
 #kui väärtus võrdub 0
 if [ $APACHE2 -eq 0 ]; then
 
-
 #väljastame teate ja paigaldame teenuse
 echo "Paigaldame apache2"
 apt install apache2
 echo "Apache on paigaldatud"
 
-
 #kui väärtus võrdub 1
 elif [ $APACHE2 -eq 1 ]; then
-
 
 #väljastame teate, et teenus on paigaldatud
 echo "Apache2 on juba paigaldatud"
@@ -32,13 +38,13 @@ if [ $MYSQL -eq 0 ]; then
 #väljastame teate ja paigaldame teenuse
 echo "Paigaldame mysql ja vajalikud lisad"
 apt install -y mysql-server
-echo "mysql on paigaldatud!"
+echo "Mysql on paigaldatud!"
 
 #kui mysql muutuja väärtus võrdub 1-ga
 elif [ $MYSQL -eq 1 ]; then
 
 #väljastame teate, et mysql on paigaldatud
-echo "mysql on juba paigaldatud!"
+echo "Mysql on juba paigaldatud!"
 
 fi
 
@@ -51,12 +57,63 @@ if [ $PHP -eq 0 ]; then
 #väljastame teate ja paigaldame teenuse
 echo "Paigaldame php ja vajalikud lisad"
 apt install -y php8.1 libapache2-mod-php8.1 php8.1-mysql
-echo "php on paigaldatud!"
+echo "Php on paigaldatud!"
 
 #kui väärtus võrdub 1-ga
 elif [ $PHP -eq 1 ]; then
 
 #siis on php paigaldatud
-echo "php on juba paigaldatud!"
+echo "Php on juba paigaldatud!"
 
 fi
+
+#Loome mysql andmebaasi ja kasutaja wordpressi jaoks
+echo "Loome MySQL andmebaasi ja kasutaja Wordpressi jaoks"
+
+read -p "Sisestage andmebaasi nimi: " ABnimi
+read -p "Sisestage MySQL kasutajanimi: " Knimi
+read -s -p "Sisestage MySQL kasutaja parool: " Kparool
+read -s -p "Sisestage MySQL root parool: " Rparool
+
+#Käivitame ja lisame andmed mysql
+mysql -u root -p"$Rparool" <<MYSQL_SCRIPT
+CREATE DATABASE $ABnimi;
+CREATE USER '$Knimi'@'localhost' IDENTIFIED BY '$Kparool';
+GRANT ALL PRIVILEGES ON $ABnimi.* TO '$Knimi'@'localhost';
+FLUSH PRIVILEGES;
+MYSQL_SCRIPT
+
+# Kinnitus
+echo "Andmebaas ja kasutaja on loodud:"
+echo "  - Andmebaas: $ABnimi"
+echo "  - Kasutaja: $Knimi"
+echo "WordPressi andmebaasi seadistamisel kasutage neid andmeid!"
+
+# Installime WP
+
+echo "Laadime alla ja installime WordPressi..."
+
+cd /tmp || exit
+wget https://wordpress.org/latest.tar.gz
+tar xzvf latest.tar.gz
+
+# Liigutame WP failid veebikausta
+
+mv wordpress/* /var/www/html/
+
+# Muudame WP failide õigusi
+
+chown -R www-data:www-data /var/www/html/
+chmod -R 777 /var/www/html/
+
+# Loome faili  wp-config.php sample failist
+
+cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
+
+# Uuendame wp-config.php andmebaasi andmetega
+
+sed -i "s/database_name_here/$dbname/" /var/www/html/wp-config.php
+sed -i "s/username_here/$dbuser/" /var/www/html/wp-config.php
+sed -i "s/password_here/$userpass/" /var/www/html/wp-config.php
+
+echo "WordPress on edukalt paigaldatud! Palun minge oma brauserisse ja avage http://192.168.42.75/install.php, et lõpule viia installimine."
